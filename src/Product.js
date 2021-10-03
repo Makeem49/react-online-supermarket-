5b6681e93d37e2ee5e4e6efad7c745eeaad05a2b
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-03 01:48:37 +0100

    adding product component

---
+++ src/Product.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import "index.css"
+
+export default function Product(props) {
+    const [count , setCount ] = useState(0);
+    let disableCount = false;
+
+    function handleCountIncreaseClick() {
+        setCount(count + 1)
+    }
+
+    function handleCountDecreaseClick() {
+        setCount(count - 1)
+    }
+
+    if (!count) {
+        disableCount = true
+    }
+
+    return <div className="product">
+        <img width="50" alt="" src={props.details.image}/>
+        <div className="product-info">
+            <h2>{props.details.name}</h2>
+            <p>{props.details.description}</p>
+        </div>
+        <div className="product-buttons">
+            <button className="product-sub" onClick={handleCountDecreaseClick } disabled={disableCount}>-</button>
+            <h3 className="product-count">{(count) ? count : null}</h3>
+            <button className="product-add" onClick={handleCountIncreaseClick }>+</button>
+        </div>
+    </div>
+}
