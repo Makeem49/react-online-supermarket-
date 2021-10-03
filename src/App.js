75a36905267ab8d20b36cc1810d1f4c3c6ba4577
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-03 01:42:54 +0100

    Adding the card to app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
-import Button from "./Button.js";
-import Link from "./Link.js";
-import Container from "./Container.js";
-import Input from "./Input.js";
-import Counter from "./Counter.js";
+// import Button from "./Button.js";
+// import Link from "./Link.js";
+// import Container from "./Container.js";
+// import Input from "./Input.js";
+// import Counter from "./Counter.js";
+// import LoginButton from './LoginButton.js'
+import Card from "./Card.js";
+import Description from "./Description.js";
 // import "App.css";
 
 
 export default function App() {
     return (
         <> 
-            <Container>
+            {/* <Container>
                 <Button type='submit' disabled='disabled'>Submit</Button>
                 <Link href='https://react-tutorial.app'>Shop Online</Link>
                 <Input type='text' placeholder='Enter your message here.' ></Input>
                 <Counter></Counter>
-            </Container>
+                <LoginButton/>
+            </Container> */}
+        
+        <Card></Card>
         </>
     )
 }
