e70846ecebacf5c3296aaa6ad47949ffa4a1d82e
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-08 02:41:57 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
-// import Button from "./Button.js";
-// import Link from "./Link.js";
-// import Container from "./Container.js";
-// import Input from "./Input.js";
-// import Counter from "./Counter.js";
-// import LoginButton from './LoginButton.js'
-import Card from "./Card.js";
-import Description from "./Description.js";
-// import "App.css";
+import Button from "./Button.js";
+import Link from "./Link.js";
+import Container from "./Container.js";
+import Input from "./Input.js";
+import LoginButton from './LoginButton.js'
+import "store-css/App.css";
 
 
 export default function App() {
     return (
         <> 
-            {/* <Container>
+            <Container>
                 <Button type='submit' disabled='disabled'>Submit</Button>
                 <Link href='https://react-tutorial.app'>Shop Online</Link>
                 <Input type='text' placeholder='Enter your message here.' ></Input>
-                <Counter></Counter>
                 <LoginButton/>
-            </Container> */}
-        
-        <Card></Card>
+            </Container>
         </>
     )
 }
