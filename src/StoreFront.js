14d32c31b616d17ccf2ba902a097237c9e6c4d07
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-18 03:25:23 +0100

    fetching data from the api

--- src/StoreFront.js
+++ src/StoreFront.js
@@ -1,56 +1,61 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Product from "./Product.js"
-import ProductForm from "./ProductForm.js";
-import ProductList from "./ProductList.js"
+import Loader from "./Loader.js";
 import "store-css/index.css"
 
 export default function StoreFront() {
     
     const [items, setItimes] = useState([]);
-    const [name , setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [messageValidate, setMessageValidate] = useState('');
+    const [isLoading , setIsLoading ] = useState(true);
+    console.log({isLoading})
 
 
+    useEffect(() => {
+        fetch('https://react-tutorial-demo.firebaseio.com/products.json')
+        .then(response => response.json() )
+        .then(data => {
+            console.log(data)
+            setItimes(data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        .finally(() => {
+            setIsLoading(false)
+        })
+    })
 
-    function handleAddProduct(event) {
-        
-        event.preventDefault()
+    {/*Fetch data using async/await*/}
 
-        if (!name) {
-            setMessageValidate('Please enter a name')
-            return null;
-        }
+    // useEffect(() => {
+    //     (
+    //         async () => {
 
-        if (!description) {
-            setMessageValidate("Please enter a description")
-            return null;
-        }
-        let addProduct = {
-            id : items.length,
-            name : name,
-            description : description,
-            image : "https://res.cloudinary.com/dbfn5lnvx/image/upload/q_auto,w_300/v1580649400/react-tutorial/products/milk.png",
-        }
-        setItimes([...items, addProduct]);
-        setName('');
-        setDescription('');
-        setMessageValidate('')
-        // console.log("done")
-    }
+    //             try {
+    //                 const response = await fetch('https://react-tutorial-demo.firebaseio.com/products.json');
+    //                 const data = await response.json()
+    //                 console.log(data)
+    //                 setItimes(data)
+    //             } catch(error) {
+    //                 console.error(error)
+    //             } finally{
+    //                 console.log('Done')
+    //             }
+    //         }
+    //     )
+    // }, [])
 
-    
-    function handleRemoveProduct(indexItem) {
-        let filteredProduct = items.filter((product, index )=> indexItem !== index)
-        setItimes([...filteredProduct])   
-    }
+    console.log(items)
+    const listItems = items.map(item => {
+        return <Product details={item} key={item.id}/>
+    })
 
     return <>
-            <ProductForm onhandleForm={handleAddProduct} name={name} description={description} messageValidate={messageValidate} setName={setName} setDescription={setDescription}/>
-
             <div className="store-front">
                 {/* render the two Products here */}
-                <ProductList items={items} onDeleteItem={handleRemoveProduct}/>
+                {(isLoading) ? <Loader />: ''}
+
+                {listItems}
             </div>;
        </> 
 }
